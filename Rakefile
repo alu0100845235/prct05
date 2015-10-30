5b4c8d42496a632ef3bbadfce6581d04662b2124
alu0100845235 <alu0100845235@ull.edu.es>
2015-10-30 12:16:01 +0000

    Añadidas pruebas para resta

--- Rakefile
+++ Rakefile
@@ -4,21 +4,25 @@ desc "Pruebas unitarias de la clase Fraccion"
 task :tu do
   sh "ruby -I. test/test1.rb"
   sh "ruby -I. test/test2.rb"
+  sh "ruby -I. test/test3.rb"
 end
 
 desc "Ejecutar solo las pruebas simples"
 task :simple do
   sh "ruby -I. test/test1.rb -n /simple/"
   sh "ruby -I. test/test2.rb -n /simple/"
+  sh "ruby -I. test/test3.rb -n /simple/"
 end
 
 desc "Ejecutar solo las pruebas de tipo"
 task :type do
   sh "ruby -I. test/test1.rb -n /type/"
   sh "ruby -I. test/test2.rb -n /type/"
+  sh "ruby -I. test/test3.rb -n /type/"
 end
 
 desc "Ejecutar solo las pruebas de division por cero"
 task :zero do
   sh "ruby -I. test/test2.rb -n /zero/"
+  sh "ruby -I. test/test3.rb -n /zero/"
 end
---
+++ test/test3.rb
@@ -0,0 +1,25 @@
+require 'lib/fraccion'
+#require '../lib/fraccion'
+require 'test/unit'
+
+class TestPoint < Test::Unit::TestCase
+  def test_simple
+    assert_equal("2/1", (Fraccion.new(4,1) - Fraccion.new(2,1)).to_s)
+    assert_equal("1/1", (Fraccion.new(4,2) - Fraccion.new(2,2)).to_s)
+    assert_equal("1/4", (Fraccion.new(1,2) - Fraccion.new(1,4)).to_s)
+    assert_equal("-7/12", (Fraccion.new(3,4) - Fraccion.new(4,3)).to_s)
+    assert_equal("2/1", (Fraccion.new(1,1) - Fraccion.new(-1,1)).to_s)
+    assert_equal("-3/4", (Fraccion.new(-1,2) - Fraccion.new(1,4)).to_s)
+  end
+  
+  def test_type_check
+    assert_raise(ArgumentError) { Fraccion.new('a','b') - Fraccion.new(1,2) }
+  end
+  
+  def test_zero_division
+    assert_raises(ZeroDivisionError) do
+      assert_equal("0/0", (Fraccion.new(0,0) - Fraccion.new(1,1)).to_s)
+    end
+  end
+  
+end
